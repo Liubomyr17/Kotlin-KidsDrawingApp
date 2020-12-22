f343d917d38bc5e20e46593c506e5920501e3171
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2020-12-22 23:28:56 +0200

    BrushSize

--- app/src/main/java/com/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/kidsdrawingapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.kidsdrawingapp
 
+import android.app.Dialog
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_brush_size.*
 
 // TODO (Step 1 : Creating a Empty Main Activity when creating a project.)
 // START
@@ -14,6 +16,30 @@ class MainActivity : AppCompatActivity() {
 
         drawing_view.setSizeForBrush(20.toFloat())
 
+        ib_brush.setOnClickListener {
+            showBrushSizeChooserDialog()
+        }
+    }
+    private fun showBrushSizeChooserDialog() {
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size: ")
+        val smallBtn = brushDialog.ib_small_brush
+        smallBtn.setOnClickListener{
+            drawing_view.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+        val mediumButton = brushDialog.ib_medium_brush
+        mediumButton.setOnClickListener{
+            drawing_view.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+        val largeBtn = brushDialog.ib_large_brush
+        largeBtn.setOnClickListener{
+            drawing_view.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+        brushDialog.show()
     }
 }
 // END
